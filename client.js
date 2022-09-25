d45d06c8aa2f791b1d001ea1a555bc2d42d12caf
nmautz <nmautz@zagmail.gonzaga.edu>
2022-09-25 10:26:37 -0700

    tmp

--- client.js
+++ client.js
@@ -1,17 +1,22 @@
-var domain = window.location.origin; //http://someurl.com
-var tmp = new URL(domain);
-tmp.port = '';
-domain = tmp.toString();
-domain = domain.substring(0, domain.length-1);
-console.log("DOmain:" + domain);
 
-let port = 3000;
-let url = `${domain}:${port}/api/getfiles`;
-window.fetch(url, {
-   method: 'GET'
-}).then(res => res.json()).then((data)=>{
+document.addEventListener("DOMContentLoaded", ()=>{
+  var domain = window.location.origin; //http://someurl.com
+  var tmp = new URL(domain);
+  tmp.port = '';
+  domain = tmp.toString();
+  domain = domain.substring(0, domain.length-1);
 
-  console.log(data);
+  let port = 3000;
+  let url = `${domain}:${port}/api/getfiles`;
+  window.fetch(url, {
+    method: 'GET'
+  }).then(res => res.json()).then((data)=>{
 
-});
+
+    console.log(data + ":data");
+    document.getElementById("test").innerHTML = data;
+    
+
+  });
+})
 
