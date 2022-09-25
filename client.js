06b2791f13203a8307d5d97cf630680530f45c23
nmautz <nmautz@zagmail.gonzaga.edu>
2022-09-25 10:55:22 -0700

    displays

--- client.js
+++ client.js
@@ -1,3 +1,24 @@
+function addFileToDisplay(fileName, fileType){
+  let parent = document.getElementById("main-display-container");
+
+  var fileDiv = document.createElement("div");
+  fileDiv.classList.add("file-display");
+  var fileimg = document.createElement("img");
+  fileimg.classList.add("file-icon-img");
+  fileimg.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/OneDrive_Folder_Icon.svg/1200px-OneDrive_Folder_Icon.svg.png";
+  fileimg.alt = "Folder"
+  var filetxt = document.createElement("p");
+  filetxt.appendChild(document.createTextNode(fileName));
+  filetxt.classList.add("file-txt");
+
+  fileDiv.appendChild(fileimg);
+  fileDiv.appendChild(filetxt);
+
+  parent.appendChild(fileDiv);
+
+
+}
+
 
 document.addEventListener("DOMContentLoaded", ()=>{
   var domain = window.location.origin; //http://someurl.com
@@ -13,9 +34,13 @@ document.addEventListener("DOMContentLoaded", ()=>{
   }).then(res => res.json()).then((data)=>{
 
 
-    console.log(data + ":data");
+    for(i = 0; i < data.length; ++i){
+      addFileToDisplay(data[i], "folder")
+
+    }
     
 
   });
+
 })
 
